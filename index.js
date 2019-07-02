f4947fd0e59261b6bb5c492eed2047dec7e4debe
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2019-07-02 09:56:34 +0200

    add session & bulma

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const path = require('path');
-
+const session = require('express-session');
 const app = express(); 
+
+
+app.use(express.static("./node_modules/bulma/css"));
+
+app.use(session({secret: 'bla_bla_secret9393'}));
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
 
 app.get('/', (req, res) => {
+    const session = req.session;
+
+    if(!session.user){
+        session.user = "Anonymous user";
+    }
+
     res.render("index", {
-        message: "Hello Everybody! Time is now: " + new Date().toISOString()
+        message: "Hello Everybody! Time is now: " + new Date().toISOString(),
+        user: session.user
     });    
 }); 
 
